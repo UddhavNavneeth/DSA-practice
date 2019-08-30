d0649d34060c64029b771abda254ca156eb6a3aa
unknown <uddhav.navneeth@gmail.com>
2019-08-30 10:02:53 +0530

    correct josephus problem with comments

--- josephus.cpp
+++ josephus.cpp
@@ -1,9 +1,9 @@
-//This is actually Linked List
 
 
 #include<iostream>
 using namespace std;
 
+//Here by returning 0 we assume that the elements in the circle start from index 0
 int josephus(int n, int k) {
 	if (n==1) {
 		return 0;
@@ -14,7 +14,8 @@ int josephus(int n, int k) {
 }
 
 int main() {
-	cout<<josephus(12,2);
+	cout<<josephus(12,2);//n=12, k=2, are just sample inputs.
+						// n is the number of people, k is kth person to be removed from the circle 
 }
 
 
