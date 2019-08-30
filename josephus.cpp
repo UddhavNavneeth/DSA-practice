098a9dafbc1f36c4445c5b813635c68ef339d7ac
unknown <uddhav.navneeth@gmail.com>
2019-08-30 10:00:41 +0530

    correct josephus problem

--- josephus.cpp
+++ josephus.cpp
@@ -4,41 +4,17 @@
 #include<iostream>
 using namespace std;
 
-class Node {
-	public:
-		int data;
-		Node* next;
-};
-
-Node* newNode(int data) {
-	Node* temp = new Node();
-	temp->data=data;
-	temp->next=NULL;
-	return temp;
-}
-
-void display(Node* temp) {
-	while (temp!=NULL) {
-		cout<<temp->data<<endl;
-		temp=temp->next;
+int josephus(int n, int k) {
+	if (n==1) {
+		return 0;
+	}
+	else {
+		return (josephus(n-1,k)+k)%n;
 	}
 }
 
 int main() {
-//	printf("%d", findLeader(8,2));
-	Node* nextNode = NULL;
-	for (int i=5; i>0; i--) {
-		Node* temp = newNode(i);
-		if (i=5) {
-			temp->next=NULL;
-		}
-		else {
-			nextNode = temp;
-		}
-		nextNode = temp;
-		
-	}
-	display(nextNode);
+	cout<<josephus(12,2);
 }
 
 
