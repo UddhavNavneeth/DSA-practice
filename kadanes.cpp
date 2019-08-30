7af8130a6b428ac412bb4ab082065e0b907e659e
unknown <uddhav.navneeth@gmail.com>
2019-08-30 10:56:39 +0530

    max run sum prob cubic time

---
+++ kadanes.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//Max contigous sub array sum
+
+void cubicTime(int array[], int n) {
+	int max=array[0];
+	for (int i=0; i<n; i++) {
+		for (int j=i;j<n;j++) {
+			int sum=0;
+			for (int k=i; k<=j; k++) {
+				sum = sum+array[k];
+			}
+			if (sum>=max) {
+				max=sum;
+			}	
+		}
+		
+	}
+	cout<<max<<endl;
+}
+
+void quadTime(int array[], int n) {
+	int max=array[0];
+	for (int i=0; i<n; i++) {
+		int runningSum=0;
+		for (int j=i; j<n ;j++) {
+			runningSum=runningSum+array[j];
+			if (runningSum>=max) {
+				max=runningSum;
+			}
+			
+		}
+	}
+	cout<<max<<endl;
+}
+
+int main() {
+	int array[8] = {-2,-3,4,-1,-2,1,5,-3};
+	cubicTime(array,8);
+	
+	
+	return 0;
+	
+}
