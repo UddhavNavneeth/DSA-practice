c35b20e929815ca00aaaa776241a9fab2fdb3226
unknown <uddhav.navneeth@gmail.com>
2020-03-19 12:31:34 +0530

    merge Sort

---
+++ mergeSort.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+
+
+void merge(int* arr, int l, int r, int m) {
+	int i,j,k;
+	int n1 = m-l+1;
+	int n2 = r-m;
+	int L[n1], R[n2];
+	
+	for (i=0;i<n1;i++) {
+		L[i] = arr[l+i];
+	}
+	for (j=0;j<n2;j++) {
+		R[j] = arr[m+1+j];
+	}
+	
+	i=0;
+	j=0;
+	k=l;
+	
+	while (i<n1 && j<n2) {
+		if (L[i]<=R[j]) {
+			arr[k] = L[i];
+			i++;
+		} else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	
+	while (i<n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	
+	while (j<n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+	
+}
+
+void mergeSort(int* arr, int l, int r) {
+	if (l<r) {
+		int m = (l+r)/2;
+		mergeSort(arr, l, m);
+		mergeSort(arr, m+1, r);
+		merge(arr, l, r, m);
+	}
+}
+
+int main() {
+	int i,n;
+	cout<<"Size of array: ";
+	cin>>n;
+	int arr[n];
+	for (i=0;i<n;i++) {
+		cin>>arr[i];
+	}
+	mergeSort(arr,0,n-1);
+	
+	for (i=0;i<n;i++) {
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
