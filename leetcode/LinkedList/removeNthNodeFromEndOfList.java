9f7d03145a8f026420a7e4672098f7c7a0361570
unknown <uddhav.navneeth@gmail.com>
2021-03-12 12:41:10 +0530

    Adding removeNthNodeFromEndOfList.java

---
+++ leetcode/LinkedList/removeNthNodeFromEndOfList.java
@@ -0,0 +1,35 @@
+// link to the question:
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/
+// Had to see solution for 1 pass method
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        int count = 0;
+        // Advances first pointer so that the gap between first and second is n nodes apart
+        while (first != null && count < n+1) {
+            count++;
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
